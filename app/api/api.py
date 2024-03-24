3757dfa7e5de67abf75128260fab6e878ee0ac72
IluF1g <pashadurov12@gmail.com>
2024-03-24 19:33:34 +0300

    added FSM

---
+++ app/api/api.py
@@ -0,0 +1,5 @@
+from config import API_KEY
+import requests
+from handlers import user_city
+
+city_for_forecast = user_city.city
--- app/handlers.py
+++ app/handlers.py
@@ -1,10 +1,25 @@
 from aiogram import Router
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
-import app.keyboard as kb
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
+
 
 router = Router()
+class user_city(StatesGroup):
+    city = State()
+
 
 @router.message(CommandStart())
 async def start_handler(message: Message):
-    await message.answer(f"Привет, {message.from_user.first_name}", reply_markup = kb.start_kb)
+    await message.answer(f"Привет, {message.from_user.first_name}! Введите комманду /weather, для того чтобы посмотреть погоду")
+
+@router.message(Command('weather'))
+async def weather_handler(message: Message, state: FSMContext):
+    await state.set_state(user_city.city)
+    await message.answer('Введите город, для просмотра погоды в нем:')
+    
+@router.message(user_city.city)
+async def set_city(message: Message, state: FSMContext):
+    await state.update_data(city = message.text)
+    await state.clear()
